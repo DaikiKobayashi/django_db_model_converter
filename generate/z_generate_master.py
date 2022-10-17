5fef18212e8ba044fe19582749b39e02654e7420
d.kobayashi <daikikobayashi1910@gmail.com>
2022-10-17 16:13:59 +0900

    'none'

--- generate/z_generate_master.py
+++ generate/z_generate_master.py
@@ -3,6 +3,11 @@ from django.db import models
 class PlayerLevelMaster(models.Model):
     level = models.IntegerField(null=True, blank=True, default=0)
     exp = models.BigIntegerField(null=True, blank=True, default=0)
+    test1 = models.BooleanField(null=True, blank=True)
+    test2 = models.TextField(null=True, blank=True, max_length=100)
+    test3 = models.DateTimeField(null=True, blank=True, default='2001/1/20')
+    test4 = models.FloatField(null=True, blank=True, default=0.001)
+    test5 = models.PositiveIntegerField(null=True, blank=True, default=1000)
 
     class Meta:
         app_label = 'app'
